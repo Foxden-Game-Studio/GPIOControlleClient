4a6a68c8d2bbb947e63fee3a03863169f8efb988
Shadow27 <shad0w27.66242007@gmail.com>
2024-12-19 14:37:13 +0100

    remaking GPIO Server Client

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import GPIOServer
 
-if __name__ == "__main__":
-    try:
-        app = GPIOServer.GPIOServerApp()
-        app.getRoot().mainloop()
-    except KeyboardInterrupt:
-        print("Terminated by User!")
